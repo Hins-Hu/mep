Fix bugs in mep computation.

main.py
@@ -6,19 +6,58 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 
+city = 'Chattanooga'
+state = 'TN'
+
+# Set the categories of activities
+"""
+Category 1: Religious facility/Schools/Day care
+Category 2: Health care
+Category 3: Retails
+Category 4: Sports & entertainment
+Category 5: Food & restaurant
+"""
+#* Need to mannually input all relevant activities according to OSM features
+C1 = ['[amenity=college]', '[amenity=driving_school]', '[amenity=kindergarten]', '[amenity=language_school]', '[amenity=music_school]',
+      '[amenity=school]', '[amenity=research_institute]', '[amenity=university]', '[amenity=place_of_worship]']
+
+C2 = ['[healthcare]', '[amenity=baby_hatch]', '[amenity=clinic]', '[amenity=dentist]', '[amenity=doctors]', '[amenity=hospital]',
+      '[amenity=nursing_home]', '[amenity=pharmacy]', '[amenity=veterinary]', '[amenity=social_facility]']
+
+C3 = ['[shop]']
+
+C4 = ['[leisure]', '[sport]', '[tourism]', '[amenity=arts_centre]', '[amenity=casino]', '[amenity=cinema]', '[amenity=community_centre]', '[amenity=conference_centre]',
+      '[amenity=events_venue]', '[amenity=exhibition_centre]', '[amenity=fountain]', '[amenity=gambling]', '[amenity=music_venue]',
+      '[amenity=nightclub]', '[amenity=planetarium]', '[amenity=studio]', '[amenity=theatre]', '[amenity=social_centre]', '[amenity=public_bookcase]']
+
+C5 = ['[amenity=bar]', '[amenity=biergarten]', '[amenity=cafe]', '[amenity=fast_food]', '[amenity=food_court]', '[amenity=ice_cream]', '[amenity=pub]',
+      '[amenity=restaurant]']
+activities = [C1, C2, C3, C4, C5]
 
 
-# Parameters
-isochrones = [5, 10, 15, 20]
+# Count the total number of opportunities of different activities in Chattanooga
+N = np.zeros(len(activities))
+for j, a in enumerate(activities):
+    places = utils.get_places_city_OSM(city = city, tags = a)
+    N[j] = len(places.nodes)
+
+# Set the total number of benchmark opportunities
+N_star = N[-1]
+    
+# The trip frequencies
+freq = [0.031, 0.015, 0.195, 0.084, 0.067]
+
+# The travel mode
 modes = ['multimodal']
-city = 'Chattanooga, TN'
 
+# The energy coefficient
+e = 0.65
 
+# The cost coefficient
+c = 1.05
 
-activities = [['[Healthcare]','[amenity=restaurant]']]
-
-N_star = ...
-freqencies = []
+# Travel time are in minutes
+isochrones = [5, 10, 15, 20]
 
 
 
@@ -38,12 +77,14 @@ url_TIGERweb = "https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/ti
 
 
 
-blocks = utils.get_census_blocks(city)
+blocks = utils.get_census_blocks(city + ', ' + state)
 block_geoids = blocks['GEOID']
+
+
 
 # Initialize the array of opportunities
 O_itj = np.zeros((len(blocks), len(isochrones), len(activities)))
-
+mep = np.zeros(len(blocks))
 
 
 # Define the executable function for multithreading
@@ -52,7 +93,7 @@ def compute_one_block(i):
     geoid = block_geoids[i]
     
     # Monitor the progress
-    # print("Now at i = ", i)
+    print("Now at i = ", i)
     sys.stdout.flush()
     
     centroid_lat, centroid_lon = utils.get_census_block_centroid(geoid)
@@ -64,11 +105,11 @@ def compute_one_block(i):
         
         if polygon != None:
             for j, a in enumerate(activities):
-                places = utils.get_places_OSM(polygon, a)
-                if t == 0:
-                    O_itj[i][t][j] = len(places.nodes)
-                else:
-                    O_itj[i][t][j] = len(places.nodes) - O_itj[i][t-1][j]        
+                places = utils.get_places_polygon_OSM(polygon, a)
+                O_itj[i][t][j] = len(places.node_ids)
+    
+    # Compute the MEP metric
+    mep[i] = utils.mep_computation(O_itj[i, :, :], N_star, N, freq, isochrones, e, c)    
                
                
 # Enable multithreading
@@ -80,13 +121,14 @@ with ThreadPoolExecutor(max_workers = 12) as executor:
 executor.shutdown()
 
 
-# Compute the MEP metric
-# TODO: Use the correct formula once the debugging is done
-blocks['Quant'] = np.sum(O_itj, axis = (1, 2))
+# Append the computed mep metric to the blocks dataframe
+blocks['MEP'] = mep
 
+# Output data
+blocks.to_file("chatt_census_blocks.geojson", driver='GeoJSON')
 
 # Generate the plot
-utils.visualization(blocks, 'Quant')
+utils.visualization(blocks, 'MEP')
 
 
 

utils.py
@@ -29,12 +29,14 @@ def get_census_block_centroid(geoid):
 
 def get_places_city_OSM(city = "Chattanooga", tags = ["[amenity=restaurant]"]):
     
-    arg = f"area[name = {city}];"
+    arg = f"area[name = {city}]->.city;"
+    arg += "("
     for tag in tags:
-        node = "node(area)"
+        node = "node(area.city)"
         node += tag
         node += ";"
         arg += node
+    arg += ");"
     arg += "out;" 
     
     overpass_api = overpy.Overpass()
@@ -124,7 +126,7 @@ def generate_square_centroids(bbox, square_size = 0.01):
 
     
 
-def mep_computation(O_tj, N_star, N, f, isochrones, e, c, alpha = -0.5, beta = -0.08, sigma = -0.5):  
+def mep_computation(O_tj, N_star, N, freq, isochrones, e, c, alpha = -0.5, beta = -0.08, sigma = -0.5):  
     
     """_summary_
 
@@ -132,7 +134,7 @@ def mep_computation(O_tj, N_star, N, f, isochrones, e, c, alpha = -0.5, beta = -
         O_tj (np.ndarray): O_tj[t][j] =  # of opportunities of type j in the polygon within t time  
         N_star (int): A factor. The total number of benchmark opportunities across multiple cities (for example, the number of meal opportunities)
         N (np.ndarray): N[j] = the total # of opportunities of activity j
-        f (np.ndarray): f[j] = the frequency that people access opportunities of activity j
+        freq (np.ndarray): f[j] = the frequency that people access opportunities of activity j
         isochrones (list): A list of isochrones (e.g., [10, 20, 30, 40])
         e (float): The energy intensity (kWh per passenger-mile)
         c (float): The cost (dollar per passenger-mile)
@@ -141,13 +143,13 @@ def mep_computation(O_tj, N_star, N, f, isochrones, e, c, alpha = -0.5, beta = -
         sigma (float, optional): weighing factors. Defaults to -0.5.
     """
     
-    O_t = O_tj @ (f / N) * (N_star * sum(f))
+    O_t = O_tj @ (freq / N) * (N_star * sum(freq))
     
-    M_t = np.repeat(alpha * e, len(isochrones)) + beta * isochrones + np.repeat(sigma * c, len(isochrones))
+    M_t = np.repeat(alpha * e, len(isochrones)) + beta * np.array(isochrones) + np.repeat(sigma * c, len(isochrones))
     
-    np.insert(O_t, 0, 0)
+    O_t = np.insert(O_t, 0, 0)
     mep = 0
-    for ind, o1, o2 in enumerate(zip(O_t[:-1], O_t[1:])):
+    for ind, (o1, o2) in enumerate(zip(O_t[:-1], O_t[1:])):
         mep += (o2 - o1) * np.exp(M_t[ind])
         
     return mep